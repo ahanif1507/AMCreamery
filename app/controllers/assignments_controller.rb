60cdb31e49664e025a5621a42035a3a4002c72d3
ahanif1507 <ahmadhanif1507@gmail.com>
2020-03-26 18:47:22 +0000

    created controllers

---
+++ app/controllers/assignments_controller.rb
@@ -0,0 +1,7 @@
+class AssignmentsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
 
+  get 'stores/new'
+  get 'stores/create'
+  get 'home/new'
+  get 'home/create'
+  get 'employees/new'
+  get 'employees/create'
+  get 'assignments/new'
+  get 'assignments/create'
+  get 'assignment/new'
+  get 'assignment/create'
   # Your routes go here
 
 end
