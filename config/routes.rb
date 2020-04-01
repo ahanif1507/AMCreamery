ef3dff7aac5d322bf0a49a0e4069837abbd2c8df
ahanif1507 <ahmadhanif1507@gmail.com>
2020-04-01 07:58:55 +0000

    created controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
     
+  get 'stores/index'
+  get 'stores/new'
+  get 'stores/show'
+  get 'stores/edit'
+  get 'assignments/index'
+  get 'assignments/new'
+  get 'employees/index'
+  get 'employees/new'
+  get 'employees/show'
+  get 'employees/edit'
+  get 'home/index'
+  get 'home/about'
+  get 'home/contact'
+  get 'home/privacy'
+  
   # Semi-static page routes
   get 'home', to: 'home#index', as: :home
   get 'home/about', to: 'home#about', as: :about
